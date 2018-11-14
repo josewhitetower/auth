e6410cdbec3ca79ab5bfef45a7dd84518bdd24bf
Jose Torreblanca <josewhitetower@gmail.com>
2018-11-14 22:08:57 +0100

    my first commit

---
+++ server/build/precache-manifest.ae5a5dc10e66bb872a1f8f0498c094af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c412ab475698b5d92c4dd63a4d0307d",
+    "url": "/static/media/NotFound.4c412ab4.png"
+  },
+  {
+    "revision": "f2a477647f14563e836bae405c0c3f6b",
+    "url": "/static/media/Login.f2a47764.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a081b17e7e937877bdc8",
+    "url": "/static/js/main.a081b17e.chunk.js"
+  },
+  {
+    "revision": "b683aee645f2bb6c72a5",
+    "url": "/static/js/1.b683aee6.chunk.js"
+  },
+  {
+    "revision": "a081b17e7e937877bdc8",
+    "url": "/static/css/main.f5fb48dc.chunk.css"
+  },
+  {
+    "revision": "3df15db2053b3a7e80e7bffa928110cf",
+    "url": "/index.html"
+  }
+];
